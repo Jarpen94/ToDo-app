5287e48042ca84ef05c35edc8fa1f2e25ace04b6
Jarpen94 <kwiatowskijarek@o2.pl>
2018-11-27 15:15:54 +0100

    Updates

---
+++ precache-manifest.ba0b7a7db6ebb85729026bb5f2af742f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "390342e39743f60a8b36",
+    "url": "/todo-app/static/js/main.390342e3.chunk.js"
+  },
+  {
+    "revision": "4704233300d6bfb7b9aa",
+    "url": "/todo-app/static/js/1.47042333.chunk.js"
+  },
+  {
+    "revision": "7ea816ce3fb23160370e257c205bfe59",
+    "url": "/todo-app/index.html"
+  }
+];
