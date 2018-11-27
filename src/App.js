acda4dd974dddc6907d67630a6caf2cd65d8e30e
Jarpen94 <kwiatowskijarek@o2.pl>
2018-11-27 10:30:19 +0100

    building list

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField';
+
 
 class App extends Component {
+
+  state = {
+    tasks: [
+      { taskName: 'Sprzątanie', completed: false },
+      { taskName: 'Odkurzanie', completed: false }
+    ],
+    taskName: ''
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <TextField hintText="Dodaj" />
+          <RaisedButton label="Dodaj" primary={true} />
+        </div>
+        {this.state.tasks.map((task, index) => (
+          <div>{task.taskName} </div>
+        )
+        )}
+
+
+
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
